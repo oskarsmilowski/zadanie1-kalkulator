dffe4b6380ce9544b5a86894abac6b45e3b657a1
Oskar Śmiłowski <69815@student.ue.poznan.pl>
2022-03-18 11:30:57 +0100

    deklaracja funkcji "dodaj"

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 def hello(name):
     return f"Hello {name}"
 
+def dodaj() -> float:
+    print('Podaj pierwszą liczbę:')
+    a = input
+    print('Podaj drugą liczbę: ')
+    b = input
+    return a + b
+
+
 print(hello("Oskar"))
