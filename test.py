2856bd322fbdc3a0d6bd8974d1fc8a4d012defe5
Oskar Śmiłowski <69815@student.ue.poznan.pl>
2022-03-18 13:41:30 +0100

    dodanie menu kalkulatora

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ def main():
     print("Podaj swoje imię: ")
     imie = input()
     print(hello(imie))
+    print("Wybierz co chcesz zrobić?\n 1 - Dodaj dwie liczby \n 2 - Odejmij dwie liczby \n 9 - Zakończ")
+    wybor = input()
+    print(f"Twój wybór: {wybor}")
 
 
 if __name__ == '__main__':
