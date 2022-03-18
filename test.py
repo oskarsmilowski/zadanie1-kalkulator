6a88c5b4eb14bbd3f9d48906ca0343efacf900b9
Oskar Śmiłowski <69815@student.ue.poznan.pl>
2022-03-18 14:21:25 +0100

    konwersja inputow w kalkulatorze na typ float

--- test.py
+++ test.py
@@ -1,18 +1,13 @@
-def hello(name):
-    return f"Hello {name}"
-
 def dodaj() -> float:
     print('Podaj pierwszą liczbę:')
-    a = input()
+    a = float(input())
     print('Podaj drugą liczbę: ')
-    b = input()
-    return a + b
+    b = float(input())
+    return a+b
 
 def odejmij() -> float:
     print('Podaj liczbę od której chcesz odjąć')
-    a = input()
+    a = float(input())
     print('Podaj liczbę odejmowaną')
-    b = input()
-    return float(a) - float(b)
-
-print(hello("Oskar"))
+    b = float(input())
+    return a-b
