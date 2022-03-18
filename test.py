4683ca2a831855243b007c14af9a21d66f2303d2
Oskar Śmiłowski <69815@student.ue.poznan.pl>
2022-03-18 13:45:45 +0100

    dodanie funkcji odejmowania

--- test.py
+++ test.py
@@ -3,10 +3,16 @@ def hello(name):
 
 def dodaj() -> float:
     print('Podaj pierwszą liczbę:')
-    a = input
+    a = input()
     print('Podaj drugą liczbę: ')
-    b = input
+    b = input()
     return a + b
 
+def odejmij() -> float:
+    print('Podaj liczbę od której chcesz odjąć')
+    a = input()
+    print('Podaj liczbę odejmowaną')
+    b = input()
+    return float(a) - float(b)
 
 print(hello("Oskar"))
