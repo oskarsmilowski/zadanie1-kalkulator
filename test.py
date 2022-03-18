c51c170dfa149a358ce12d31c2790ce043e47984
Oskar Śmiłowski <69815@student.ue.poznan.pl>
2022-03-18 14:15:15 +0100

    zmiana kolejności funkcji

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
+def hello(name):
+    return f"Hello {name}"
+
+def dodaj() -> float:
+    print('Podaj pierwszą liczbę:')
+    a = input()
+    print('Podaj drugą liczbę: ')
+    b = input()
+    return a + b
+
+def odejmij() -> float:
+    print('Podaj liczbę od której chcesz odjąć')
+    a = input()
+    print('Podaj liczbę odejmowaną')
+    b = input()
+    return float(a) - float(b)
+
 def main():
     print("Podaj swoje imię: ")
     imie = input()
@@ -19,20 +36,5 @@ if __name__ == '__main__':
     main()
 
 
-def hello(name):
-    return f"Hello {name}"
 
-def dodaj() -> float:
-    print('Podaj pierwszą liczbę:')
-    a = input()
-    print('Podaj drugą liczbę: ')
-    b = input()
-    return a + b
-
-def odejmij() -> float:
-    print('Podaj liczbę od której chcesz odjąć')
-    a = input()
-    print('Podaj liczbę odejmowaną')
-    b = input()
-    return float(a) - float(b)
 
