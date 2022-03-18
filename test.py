77874f3b2c4d875caa41f68148a7de8a980ce284
Oskar Śmiłowski <69815@student.ue.poznan.pl>
2022-03-18 14:25:27 +0100

    poprawki w printach + funkcja hello

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+def hello(name):
+    return f"Hello {name}"
+
 def dodaj() -> float:
     print('Podaj pierwszą liczbę:')
     a = float(input())
@@ -16,7 +19,7 @@ def main():
     print("Podaj swoje imię: ")
     imie = input()
     print(hello(imie))
-    print("Wybierz co chcesz zrobić?\n 1 - Dodaj dwie liczby \n 2 - Odejmij dwie liczby \n 9 - Zakończ")
+    print("Wybierz co chcesz zrobić?\n 1 - Dodaj dwie liczby\n 2 - Odejmij dwie liczby\n 9 - Zakończ")
     wybor = int(input())
     if wybor == 1:
         wynik = dodaj()
